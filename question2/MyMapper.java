0c106a0a01a968e5323bd29747f2b7bd09714d7b
yash044 <64230285+yash044@users.noreply.github.com>
2021-06-17 11:33:32 +0530

    Add files via upload

---
+++ question2/MyMapper.java
@@ -0,0 +1,26 @@
+// exception handling 
+import java.io.IOException;
+
+// box classes import
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+// import mapper class
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+		String inputstring = value.toString();
+		for (String x : inputstring.split(" ")) {
+			if(x.equals("India")) {			
+				context.write(new Text(x), new IntWritable(1));
+			}
+		}
+	}
+}
